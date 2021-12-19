c8d61b811a5f3feabcb71c7a230274c8874ed337
binotation <68807919+binotation@users.noreply.github.com>
2021-12-19 17:16:45 +1000

    auth endpoint

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,15 +1,46 @@
-import express, { Application } from 'express'
+import express, { Application, Request, Response } from 'express'
+import { OutgoingHttpHeaders } from 'http'
 import path from 'path'
-import { Config } from './types'
+import { Config, ResponseMessage } from './types'
 
 const config: Config = require('../config.json')
 const app: Application = express()
 
 app.use(express.static(path.join(__dirname, config.buildPath)))
 app.use(express.static(path.join(__dirname, config.publicPath)))
+app.use(express.urlencoded({ extended: true }))
 
-app.post('/submit', (req, res) => {
-    res.send('This was a successful post request.')
+app.post('/auth', (req: Request, res: Response) => {
+    const headers: OutgoingHttpHeaders = { 'Content-Type': 'application/json' }
+    let status: number = 500
+    const responseMessage: ResponseMessage = {}
+
+    const params: URLSearchParams = new URLSearchParams(req.body)
+    const id: string = params.get('id') ?? ''
+    const token: string = params.get('token') ?? ''
+
+    if ([id, token].includes('')) {
+        status = 400
+        responseMessage.error = 'Missing mandatory parameters.'
+
+    } else {
+        const clientTokens: any = {}
+        config.registeredClients.forEach(client => {
+            clientTokens[client.id] = client.token
+        })
+        
+        if (clientTokens[id] === token) {
+            status = 200
+            responseMessage.authorized = true
+        } else {
+            status = 401
+            responseMessage.authorized = false
+        }
+    }
+
+    res.set(headers)
+    res.status(status)
+    res.send(JSON.stringify(responseMessage))
 })
 
 app.listen(config.port, () => {
--- server/src/types.ts
+++ server/src/types.ts
@@ -1,5 +1,11 @@
 export interface Config {
     port: number,
     buildPath: string,
-    publicPath: string
+    publicPath: string,
+    registeredClients: { id: string, token: string }[]
+}
+
+export interface ResponseMessage {
+    error?: string,
+    authorized?: boolean
 }
