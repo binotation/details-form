bcc6704c675ecb842268e77e434f4a0aeb02e86b
binotation <68807919+binotation@users.noreply.github.com>
2021-12-19 20:02:32 +1000

    Changed auth endpoint to receive json instead of urlencoded-form

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,16 +8,15 @@ const app: Application = express()
 
 app.use(express.static(path.join(__dirname, config.buildPath)))
 app.use(express.static(path.join(__dirname, config.publicPath)))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
 
 app.post('/auth', (req: Request, res: Response) => {
     const headers: OutgoingHttpHeaders = { 'Content-Type': 'application/json' }
     let status: number = 500
     const responseMessage: ResponseMessage = {}
 
-    const params: URLSearchParams = new URLSearchParams(req.body)
-    const id: string = params.get('id') ?? ''
-    const token: string = params.get('token') ?? ''
+    const id: string = req.body['id'] ?? ''
+    const token: string = req.body['token'] ?? ''
 
     if ([id, token].includes('')) {
         status = 400
