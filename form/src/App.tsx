7297593f53e121314bbb63fd5ff703588b4592c2
binotation <68807919+binotation@users.noreply.github.com>
2021-12-20 10:59:43 +1000

    Added load on authorization

--- form/src/App.tsx
+++ form/src/App.tsx
@@ -1,26 +1,74 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import CircularProgress from '@mui/material/CircularProgress'
+import { LoadingState } from './types'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const qs: URLSearchParams = new URLSearchParams(window.location.search)
+    const [loadingState, setLoadingState] = useState(LoadingState.Loading)
+
+    useEffect(() => {
+        const body: { id: string, token: string } = {
+            id: qs.get('id') ?? '',
+            token: qs.get('token') ?? ''
+        }
+
+        if (body.id === '' || body.token === '') {
+            setLoadingState(LoadingState.Error)
+            return;
+        } else {
+            fetch('auth', {
+                method: 'POST',
+                mode: 'same-origin',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(body)
+            })
+                .then(resp => {
+                    switch (resp.status) {
+                        case 401: {
+                            setLoadingState(LoadingState.Unauthorized)
+                            break
+                        }
+                        case 200: {
+                            setLoadingState(LoadingState.Authorized)
+                            break
+                        }
+                        default: {
+                            setLoadingState(LoadingState.Error)
+                        }
+                    }
+                })
+        }
+    })
+
+    const loadingRender = () => {
+        switch (loadingState) {
+            case LoadingState.Error: {
+                return <h1>{LoadingState.Error}</h1>
+            }
+            case LoadingState.Authorized: {
+                return <h1>{LoadingState.Authorized}</h1>
+            }
+            case LoadingState.Unauthorized: {
+                return <h1>{LoadingState.Unauthorized}</h1>
+            }
+            case LoadingState.Loading: {
+                return <CircularProgress size={56} />
+            }
+        }
+    }
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                {loadingRender()}
+            </header>
+        </div>
+    )
 }
 
 export default App;
---
+++ form/src/types.ts
@@ -0,0 +1,6 @@
+export enum LoadingState {
+    Loading = 'Loading...',
+    Unauthorized = 'Unauthorized',
+    Authorized = 'Authorized',
+    Error = 'Error'
+}
