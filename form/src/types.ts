0b092cdf973efd018cab9c71b0c118eb7d0fcd7a
binotation <68807919+binotation@users.noreply.github.com>
2021-12-21 13:58:37 +1000

    added form values interface and enums

--- form/src/types.ts
+++ form/src/types.ts
@@ -4,3 +4,66 @@ export enum LoadingState {
     Authorized = 'Authorized',
     Error = 'Error'
 }
+
+export enum AddressState {
+    ACT, NSW, NT, QLD, SA, TAS, VIC, WA
+}
+
+export enum SuperChoice {
+    APRA, SMSF, EmployerNominated, None
+}
+
+export enum EmploymentType {
+    FullTime, PartTime, Casual, SuperIncomeStream, LabourHire
+}
+
+export enum ResidencyStatus {
+    AusOrNZ, Overseas, NoWorkRights
+}
+
+export interface FormValues {
+    FirstName: string,
+    LastName: string,
+    Email: string,
+    Phone: string,
+
+    AddressLine: string,
+    AddressSuburb: string,
+    AddressState: AddressState | '',
+    AddressPostalCode: string,
+
+    EmergencyContactName: string,
+    EmergencyContactPhone: string,
+    EmergencyContactRelationship: string,
+
+    BankAccountName: string,
+    BankAccountNumber: string,
+    BankBSB: string,
+
+    SuperChoice: SuperChoice | '',
+    StapledSuper: boolean,
+    APRAUSI: string,
+    APRAMemberNumber: string,
+    SMSFName: string,
+    SMSFABN: string,
+    SMSFAccountName: string,
+    SMSFAccountNumber: string,
+    SMSFBSB: string,
+    SMSFElectronicServiceAddress: string,
+    SuperConfirmed: boolean,
+
+    TFN: string,
+    DateOfBirth: Date | '',
+    EmployeePaidBasis: EmploymentType | '',
+    IsAusResidentForTaxPurposes: boolean | '',
+    TaxFreeThresholdClaimed: boolean | '',
+    SeniorsPensioners: boolean | '',
+    TaxZoneOverseasInvalidCarer: boolean | '',
+    HasHelpDebt: boolean | '',
+    HasSupplementDebt: boolean | '',
+    TaxConfirmed: boolean,
+
+    ResidencyStatus: ResidencyStatus | '',
+    Convicted: boolean | '',
+    ConvictionComment: string
+}
