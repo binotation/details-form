afe95353e4a7028e62eb427480daa2289f2a8c6f
binotation <68807919+binotation@users.noreply.github.com>
2021-12-21 16:47:39 +1000

    added Emergency contact and Bank account information to PersonalDetails

--- form/src/form-sections/PersonalDetails.tsx
+++ form/src/form-sections/PersonalDetails.tsx
@@ -6,18 +6,30 @@ function PersonalDetails({ control }: { control: any }) {
         <div className='FieldGroup FormSection'>
             <h2>Personal Details</h2>
             <FormGroup>
-                <h3>Contact</h3>
+                <h3>Contact Information</h3>
                 <TextInput name='FirstName' control={control} label={'First Name'} />
                 <TextInput name='LastName' control={control} label={'Last Name'} />
                 <TextInput name='Email' control={control} label={'Email'} />
                 <TextInput name='Phone' control={control} label={'Phone Number'} />
             </FormGroup>
             <FormGroup>
-                <h3>Address</h3>
+                <h3>Home Address Information</h3>
                 <TextInput name='AddressLine' control={control} label={'Address Line'} />
                 <TextInput name='AddressSuburb' control={control} label={'Suburb'} />
                 <TextInput name='AddressState' control={control} label={'State'} />
                 <TextInput name='AddressPostalCode' control={control} label={'Postcode'} />
+            </FormGroup>
+            <FormGroup>
+                <h3>Emergency Contact Information</h3>
+                <TextInput name='EmergencyContactName' control={control} label={'Name'} />
+                <TextInput name='EmergencyContactPhone' control={control} label={'Phone'} />
+                <TextInput name='EmergencyContactRelationship' control={control} label={'Relationship to Emergency Contact'} />
+            </FormGroup>
+            <FormGroup>
+                <h3>Bank Account Information</h3>
+                <TextInput name='BankAccountName' control={control} label={'Account Name'} />
+                <TextInput name='BankAccountNumber' control={control} label={'Account Number'} />
+                <TextInput name='BankBSB' control={control} label={'BSB'} />
             </FormGroup>
         </div>
     )
