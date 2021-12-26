9c930defaf9d0cc04cb15382653fa7488b2676d6
binotation <68807919+binotation@users.noreply.github.com>
2021-12-26 17:38:01 +1000

    .gitignore: ignore *.db README.md: setup instructions flyway.conf: added V1__initial.sql: version 1 db script

---
+++ README.md
@@ -0,0 +1,19 @@
+<u>Setup</u>
+
+1. Build form by running
+
+   ```bash
+   details-form/form $ npm run build
+   ```
+
+   in form/
+
+2. Set up sqlite database using the scripts in database/scripts/ in the given order.
+
+3. Start up server by using
+
+   ```bash
+   details-form/server $ npm start
+   ```
+
+   in server/
---
+++ database/scripts/V1__initial.sql
@@ -0,0 +1,117 @@
+PRAGMA foreign_keys = ON;
+
+create table if not exists main.ref_state (
+    Id integer primary key,
+    DisplayName text
+);
+
+create table if not exists main.ref_super_choice (
+    Id integer primary key,
+    DisplayName text
+);
+
+create table if not exists main.ref_residency_status (
+    Id integer primary key,
+    DisplayName text
+);
+
+create table if not exists main.ref_employment_type (
+    Id integer primary key,
+    DisplayName text
+);
+
+insert into ref_state
+values
+    (0, "ACT"),
+    (1, "NSW"),
+    (2, "NT"),
+    (3, "QLD"),
+    (4, "SA"),
+    (5, "TAS"),
+    (6, "VIC"),
+    (7, "WA")
+;
+
+insert into ref_super_choice
+values
+    (0, "APRA fund or retirement savings account (RSA)"),
+    (1, "Self-managed super fund (SMSF)"),
+    (2, "Super fund nominated by my employer")
+;
+
+insert into ref_residency_status
+values
+    (0, "Australia or New Zealand Resident"),
+    (1, "Foreign Resident"),
+    (2, "Working Holiday Maker")
+;
+
+insert into ref_employment_type
+values
+    (0, "Full-time employment"),
+    (1, "Part-time employment"),
+    (2, "Casual employment"),
+    (3, "Labour Hire"),
+    (4, "Superannuation or annuity income stream")
+;
+
+create table person (
+    Id integer primary key,
+    FirstName text not null,
+    LastName text not null,
+    Email text not null,
+    Phone text not null,
+
+    AddressLine text not null,
+    AddressSuburb text not null,
+    AddressState integer not null ,
+    AddressPostalCode text not null,
+
+    EmergencyContactName text not null,
+    EmeregncyContactPhone text not null,
+    EmergencyContactRelationship text not null,
+
+    BankAccountName text not null,
+    BankAccountNumber text not null,
+    BankBSB text not null,
+
+    SuperChoice integer not null,
+    StapledSuper integer not null,
+    APRAUSI text not null,
+    APRAMemberNumber text not null,
+    SMSFName text not null,
+    SMSFABN text not null,
+    SMSFAccountName text not null,
+    SMSFAccountNumber text not null,
+    SMSFBSB text not null,
+    SMSFElectronicServiceAddress text not null,
+    SuperConfirmed integer not null,
+
+    TFN text not null,
+    DateOfBirth text not null,
+    EmployeePaidBasis integer not null,
+    IsAusResidentForTaxPurposes integer not null,
+    TaxFreeThresholdClaimed integer not null,
+    SeniorsPensioners integer not null,
+    TaxZoneOverseasInvalidCarer integer not null,
+    HasHelpDebt integer not null,
+    HasSupplementDebt integer not null,
+    TaxConfirmed integer not null,
+
+    ResidencyStatus integer not null,
+    Convicted integer not null,
+    ConvictionComment text not null,
+
+    foreign key(AddressState) references ref_state(Id)
+    foreign key(SuperChoice) references ref_super_choice(Id)
+    foreign key(EmployeePaidBasis) references ref_employment_type(Id),
+    foreign key(ResidencyStatus) references ref_residency_status(Id)
+);
+
+create table submit_token (
+    token integer primary key,
+    person integer null,
+    date_created text not null,
+
+    foreign key(person) references person(Id)
+);
