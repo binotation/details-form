273f5d9e3c3e436af70a7a60b2e5812c6fcab742
binotation <68807919+binotation@users.noreply.github.com>
2022-01-04 19:50:28 +1000

    Wrapped submit button inside Box

--- form/src/form-sections/FormButtons.tsx
+++ form/src/form-sections/FormButtons.tsx
@@ -1,10 +1,19 @@
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
 
 function FormButtons({ handleSubmit, id, token }: { handleSubmit: any, id: string, token: string }) {
     const onSubmit = (data: any) => { console.log(data) }
 
+    const style = {
+        marginTop: '36px',
+        display: 'flex',
+        justifyContent: 'center'
+    }
+
     return (
-        <Button variant='text' onClick={handleSubmit(onSubmit)}>Submit</Button>
+        <Box sx={style}>
+            <Button variant='contained' onClick={handleSubmit(onSubmit)}>Submit</Button>
+        </Box>
     )
 }
 
